e86eb7a57487b0d85784cf311b1ed49d7db91e96
GaoHongchen <1656738573@qq.com>
2017-10-25 18:41:00 +0800

    add TEST for FindShiTomasiScoreAtPoint

--- UnitTest/TImgProc.cpp
+++ UnitTest/TImgProc.cpp
@@ -2,9 +2,20 @@
 
 #include "ImgProc.h"
 
+#include <iostream>
+
 TEST(ImgProc,DetectFASTCorners10)
 {
     cv::Mat srcImage = cv::imread("/home/gordon/Pictures/47.jpg");
     std::vector<cv::Point2f> vecKeyPoints;
-    ImgProc::DetectFASTCorners10(srcImage, vecKeyPoints, 10);
+    ImgProc::DetectFASTCorners(srcImage, vecKeyPoints, 10, true);
+}
+
+TEST(ImgProc,FindShiTomasiScoreAtPoint)
+{
+    cv::Mat srcImage = cv::imread("/home/gordon/projects/SLAM/PTAM4AR/data/rgbd_dataset_freiburg1_xyz/rgb/1305031102.311267.png");
+    cv::Mat imgBW;
+    cv::cvtColor(srcImage,imgBW, CV_BGR2GRAY);
+    double score = ImgProc::FindShiTomasiScoreAtPoint(imgBW, cv::Point2i(215,278), 3);
+    std::cout << "score: " << score << std::endl;
 }
